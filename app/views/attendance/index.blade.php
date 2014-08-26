e33e6529d7f76c991bbb845ea174365ba5dc178e
shahedruhi <shahedruhi@gmail.com>
2014-08-26 12:41:04 +0600

    start creating views

---
+++ app/views/attendance/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LAB
+ * Date: 8/26/14
+ * Time: 12:40 PM
+ */ 
---
+++ app/views/layouts/attendance.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LAB
+ * Date: 8/26/14
+ * Time: 12:39 PM
+ */ 
