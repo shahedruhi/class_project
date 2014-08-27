73050f5cbeab2fdcb454a45897d770e1901685b6
shahedruhi <shahedruhi@gmail.com>
2014-08-27 13:15:12 +0600

    creating views

--- app/views/attendances/page1.blade.php
+++ app/views/attendances/page1.blade.php
@@ -9,49 +9,74 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-xs-12 center" style="text-align: left;">
-                <form class="form-inline" role="form">
-                    <div class="dropdown">
-                        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown">
-                            Dropdown
-                            <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
-                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
-                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
-                        </ul>
-                        <form class="form-inline" role="form">
-                            <div class="form-group">
-                                <div class="dropdown">
-                                    <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown">
-                                        Dropdown
-                                        <span class="caret"></span>
-                                    </button>
-                                    <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-                                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
-                                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
-                                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="input-group">
-                                    <div class="input-group-addon">@</div>
-                                    <input class="form-control" type="email" placeholder="Enter email">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="sr-only" for="exampleInputPassword2">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
-                            </div>
-                            <div class="checkbox">
+
+
+                {{ Form::open(array('url' => url('foo/bar'), 'class'=>'form-inline')) }}
+
+                {{ Form::label('selectBatch', 'SELECT BATCH') }}
+                {{ Form::select('selectBatch', array('select', 'FTFL - 01', 'FTFL - 03', 'FTFL - 03', 'FTFL - 04')) }}
+                {{ Form::label('selectTrack', 'SELECT TRACK') }}
+                {{ Form::select('selectTrack', array('select', 'ITS', 'ITES', 'ITSS', 'MANAGEMENT')) }}
+
+                {{ Form::label('selectTrack', 'SELECT DATE') }}
+
+
+                {{ Form::selectRange('number', 01, 31) }}
+                {{ Form::selectMonth('month') }}
+
+
+
+                {{ Form::selectYear('year', 2014, 2018) }}
+
+
+
+                {{ Form::close() }}
+
+                <br>
+                <br>
+
+                <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Presence</th>
+
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>ITS-2014000023</td>
+                        <td>Sudeb Kumar</td>
+                        <td><div class="checkbox">
                                 <label>
-                                    <input type="checkbox"> Remember me
+                                    <input type="checkbox"> Present
                                 </label>
                             </div>
-                            <button type="submit" class="btn btn-default">Sign in</button>
-                        </form>
-
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>ITS-2014000023</td>
+                        <td>Sudeb Kumar</td>
+                        <td><div class="checkbox">
+                                <label>
+                                    <input type="checkbox"> Present
+                                </label>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>ITS-2014000023</td>
+                        <td>Sudeb Kumar</td>
+                        <td><div class="checkbox">
+                                <label>
+                                    <input type="checkbox"> Present
+                                </label>
+                            </div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
 
